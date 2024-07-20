a1e0b5a36f6a6e4649d18a0c165c6a0396fa97a6
Rahul D <rahul_d@epam.com>
2024-07-21 00:10:45 +0530

    My first commit

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -6,6 +6,15 @@ public class ElectronicWatch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
+        int hours,minutes;
+        hours = seconds / 3600;
+        seconds = seconds % 3600;
+        minutes = seconds / 60;
+        seconds = seconds % 60;
+
+        String formattedOutput = String.format("%02d:%02d:%02d",hours,minutes,seconds);
+        System.out.println(formattedOutput);
+
 
 
     }
